b3ae5638c415448f5a7f9d6700e4ae50cd9f0319
sohadQtaitat <runner@fv-az87.w1e5cg2h11hu1jcaqoqyglpg5e.bx.internal.cloudapp.net>
2020-02-25 11:04:32 +0000

    Updates

---
+++ precache-manifest.32e4794fd2927e2e8fe0092303c69747.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2c70090b115bf2dc54ac1a890edb42",
+    "url": "/lab27-React-Testing-and-Deployment/index.html"
+  },
+  {
+    "revision": "1bec8b390e9578ec3a6d",
+    "url": "/lab27-React-Testing-and-Deployment/static/css/main.dfad0b11.chunk.css"
+  },
+  {
+    "revision": "a87911ce6622a2146e87",
+    "url": "/lab27-React-Testing-and-Deployment/static/js/2.14b22cba.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/lab27-React-Testing-and-Deployment/static/js/2.14b22cba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bec8b390e9578ec3a6d",
+    "url": "/lab27-React-Testing-and-Deployment/static/js/main.709eb832.chunk.js"
+  },
+  {
+    "revision": "7a48caa29bab19e93c44",
+    "url": "/lab27-React-Testing-and-Deployment/static/js/runtime-main.65d223ae.js"
+  }
+]);
